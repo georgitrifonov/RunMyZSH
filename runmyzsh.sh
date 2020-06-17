dd4bb3fbbfd930fa81ee3f320a459e7075b2788a
Georgi Trifonov <42679919+georgitrifonov@users.noreply.github.com>
2020-06-17 11:38:52 +0300

    Update runmyzsh.sh

--- runmyzsh.sh
+++ runmyzsh.sh
@@ -5,4 +5,4 @@ chsh -s $(which zsh)
 yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-sed -i 's/^plugins=.*/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' .zshrc
+sed -i 's/^plugins=.*/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
