f69a39e699e9f2ba27bab5d3f3b9d382751cda5d
Georgi Trifonov <42679919+georgitrifonov@users.noreply.github.com>
2021-11-17 12:20:06 +0200

    update with bat

--- runmyzsh.sh
+++ runmyzsh.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-apt update && apt install curl git zsh -y
+apt update && apt install curl git zsh bat -y
 chsh -s $(which zsh)
 yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 sed -i 's/^plugins=.*/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
+echo -e "alias cat='batcat --paging=never --style=plain'\n" >> ~/.zshrc
